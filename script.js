169efac8b2a12830f878e61fe8ace58b323fa5ff
Mauro Lobo <105966365+devmaurolobo@users.noreply.github.com>
2023-09-18 17:09:12 -0300

    18/09 17.10
    18/09 17.10

--- script.js
+++ script.js
@@ -29,7 +29,7 @@
     ".rodape p, #color-light, #color-black, .acordion-text, " +
     "#acordion-text, .cartao1, .proj1, .cartao2, .proj2, " +
     ".cartao2, .proj2, .proj3, .cartao3, .proj4, .cartao4, " +
-    ".proj5, .cartao5,.black_light_btn, #menu-list");
+    ".proj5, .cartao5,.black_light_btn, #menu-list, .title");
 
     elementsToToggle.forEach(function(elem) {
         elem.classList.toggle("dark-mode");
